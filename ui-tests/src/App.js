6650d547a6f30b4e0d56ffc9ef0ce0142b900474
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2021-05-12 10:28:49 +0500

    ui-tests remaining stuff

--- ui-tests/src/App.js
+++ ui-tests/src/App.js
@@ -1,10 +1,18 @@
-import "./App.css";
-import AutoComplete from "./Components/Autocomplete";
+// import "./App.css";
+import CustomAutoComplete from "./Components/Autocomplete";
+import { Form } from "antd";
+const { Item, useForm } = Form;
 
 function App() {
+  const [form] = useForm();
   return (
     <div className="App">
-      <AutoComplete />
+      {/* <h1>TEST</h1> */}
+      <Form form={form}>
+        <Item name="assignee" label="Assignee">
+          <CustomAutoComplete />
+        </Item>
+      </Form>
     </div>
   );
 }
--- ui-tests/src/Components/Autocomplete/index.js
+++ ui-tests/src/Components/Autocomplete/index.js
@@ -1,12 +1,37 @@
 import debounce from "lodash/debounce";
 import { AutoComplete } from "antd";
+import { useCallback, useState } from "react";
 
 const AutoComp = ({ value, onChange }) => {
-  
+  const [data, setData] = useState([]);
+
+  const handleChange = (v) => {
+    console.log({ v });
+  };
+  const onSearch = useCallback(
+    debounce(async (v) => {
+      const a = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+      const result = await a.json();
+      setData(
+        result.map((r) => ({
+          value: r.id,
+          label: r.title,
+        }))
+      );
+      // console.log({ body: a.json() });
+    }, 800),
+    []
+  );
+  console.log({ data });
   return (
     <div>
-      <h1>AutoComplete</h1>
-      <AutoComplete />
+      <AutoComplete
+        onChange={handleChange}
+        allowClear
+        mode="combobox"
+        options={data}
+        onSearch={onSearch}
+      />
     </div>
   );
 };
